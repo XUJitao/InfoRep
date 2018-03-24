11fd66151d7bdef2e8039605e52637536efc445a
Xu Jitao <jitao.xu@insa-rouen.fr>
2018-03-24 22:32:59 +0100

    modify Product.java

--- RMI/src/ProductImpl.java
+++ RMI/src/ProductImpl.java
@@ -3,17 +3,22 @@ import java.rmi.RemoteException;
 public class ProductImpl implements Product {
 	private String name;
 	private int price;
+	private Bidder owner;
 	private ArrayList<Bidder> bidders;
 
 	public ProductImpl(String name, int price) {
 		this.name = name;
 		this.price = price;
+		this.owner = null;
 		this.bidders = new ArrayList<Bidder>();
 	}
 
-	public boolean setPrice(int price) throws RemoteException {
+	public boolean bid(Bidder bidder, int price) throws RemoteException {
 		if (price > this.price) {
 			this.price = price;
+			this.owner = bidder;
+			if (!this.bidders.contains(bidder))
+				this.bidders.add(bidder);
 			return true;
 		}
 		else {
@@ -23,9 +28,5 @@ public class ProductImpl implements Product {
 
 	public ArrayList<Bidder> getBidders() throws RemoteException {
 		return this.bidders;
-	}
-
-	public void addBidder(Bidder bidder) throws RemoteException {
-		this.bidders.append(bidder);
 	}
 }
