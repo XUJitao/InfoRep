ef542a5b8ec86ea2b850d33180f8e86065d7c1b1
Xu Jitao <jitao.xu@insa-rouen.fr>
2018-04-12 23:00:07 +0200

    add Client.java in client, use wsimport to get all classes in client

---
+++ SOAP/client/Client.java
@@ -0,0 +1,41 @@
+package client;
+
+import client.CalculatriceServerImplService;
+import client.CalculatriceServer;
+import java.util.Scanner;
+
+public class Client {
+	
+	public static void main(String[] args) {
+		Scanner sc;
+		CalculatriceServerImplService service = new CalculatriceServerImplService();
+		CalculatriceServer port = service.getCalculatriceServerImplPort();
+		while(true) {
+			System.out.println("Saisir la formule à calculer, séparer l'opérande et l'opération par espace");
+			sc = new Scanner(System.in);
+			String calcul = sc.nextLine();
+			if (calcul.equalsIgnoreCase("exit"))
+				break;
+			String[] composants = calcul.split(" ");
+			int operande1 = Integer.parseInt(composants[0]);
+			int operande2 = Integer.parseInt(composants[2]);
+			String operation = composants[1];
+			int result = 0;
+			switch(operation) {
+				case "+":
+					result = port.addition(operande1, operande2);
+					break;
+				case "-":
+					result = port.soustraction(operande1, operande2);
+					break;
+				case "*":
+					result = port.multiplication(operande1, operande2);
+					break;
+				case "/":
+					result = port.division(operande1, operande2);
+					break;
+			}
+			System.out.println("Le résultat est : " + result);
+		}
+	}
+}
---
+++ SOAP/client/ObjectFactory.java
@@ -0,0 +1,179 @@
+
+package client;
+
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.annotation.XmlElementDecl;
+import javax.xml.bind.annotation.XmlRegistry;
+import javax.xml.namespace.QName;
+
+
+/**
+ * This object contains factory methods for each 
+ * Java content interface and Java element interface 
+ * generated in the client package. 
+ * <p>An ObjectFactory allows you to programatically 
+ * construct new instances of the Java representation 
+ * for XML content. The Java representation of XML 
+ * content can consist of schema derived interfaces 
+ * and classes representing the binding of schema 
+ * type definitions, element declarations and model 
+ * groups.  Factory methods for each of these are 
+ * provided in this class.
+ * 
+ */
+@XmlRegistry
+public class ObjectFactory {
+
+    private final static QName _Multiplication_QNAME = new QName("http://service.SOAP/", "multiplication");
+    private final static QName _MultiplicationResponse_QNAME = new QName("http://service.SOAP/", "multiplicationResponse");
+    private final static QName _Soustraction_QNAME = new QName("http://service.SOAP/", "soustraction");
+    private final static QName _Addition_QNAME = new QName("http://service.SOAP/", "addition");
+    private final static QName _SoustractionResponse_QNAME = new QName("http://service.SOAP/", "soustractionResponse");
+    private final static QName _Division_QNAME = new QName("http://service.SOAP/", "division");
+    private final static QName _DivisionResponse_QNAME = new QName("http://service.SOAP/", "divisionResponse");
+    private final static QName _AdditionResponse_QNAME = new QName("http://service.SOAP/", "additionResponse");
+
+    /**
+     * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: client
+     * 
+     */
+    public ObjectFactory() {
+    }
+
+    /**
+     * Create an instance of {@link Division }
+     * 
+     */
+    public Division createDivision() {
+        return new Division();
+    }
+
+    /**
+     * Create an instance of {@link SoustractionResponse }
+     * 
+     */
+    public SoustractionResponse createSoustractionResponse() {
+        return new SoustractionResponse();
+    }
+
+    /**
+     * Create an instance of {@link DivisionResponse }
+     * 
+     */
+    public DivisionResponse createDivisionResponse() {
+        return new DivisionResponse();
+    }
+
+    /**
+     * Create an instance of {@link AdditionResponse }
+     * 
+     */
+    public AdditionResponse createAdditionResponse() {
+        return new AdditionResponse();
+    }
+
+    /**
+     * Create an instance of {@link Multiplication }
+     * 
+     */
+    public Multiplication createMultiplication() {
+        return new Multiplication();
+    }
+
+    /**
+     * Create an instance of {@link MultiplicationResponse }
+     * 
+     */
+    public MultiplicationResponse createMultiplicationResponse() {
+        return new MultiplicationResponse();
+    }
+
+    /**
+     * Create an instance of {@link Soustraction }
+     * 
+     */
+    public Soustraction createSoustraction() {
+        return new Soustraction();
+    }
+
+    /**
+     * Create an instance of {@link Addition }
+     * 
+     */
+    public Addition createAddition() {
+        return new Addition();
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link Multiplication }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://service.SOAP/", name = "multiplication")
+    public JAXBElement<Multiplication> createMultiplication(Multiplication value) {
+        return new JAXBElement<Multiplication>(_Multiplication_QNAME, Multiplication.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link MultiplicationResponse }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://service.SOAP/", name = "multiplicationResponse")
+    public JAXBElement<MultiplicationResponse> createMultiplicationResponse(MultiplicationResponse value) {
+        return new JAXBElement<MultiplicationResponse>(_MultiplicationResponse_QNAME, MultiplicationResponse.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link Soustraction }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://service.SOAP/", name = "soustraction")
+    public JAXBElement<Soustraction> createSoustraction(Soustraction value) {
+        return new JAXBElement<Soustraction>(_Soustraction_QNAME, Soustraction.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link Addition }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://service.SOAP/", name = "addition")
+    public JAXBElement<Addition> createAddition(Addition value) {
+        return new JAXBElement<Addition>(_Addition_QNAME, Addition.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link SoustractionResponse }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://service.SOAP/", name = "soustractionResponse")
+    public JAXBElement<SoustractionResponse> createSoustractionResponse(SoustractionResponse value) {
+        return new JAXBElement<SoustractionResponse>(_SoustractionResponse_QNAME, SoustractionResponse.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link Division }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://service.SOAP/", name = "division")
+    public JAXBElement<Division> createDivision(Division value) {
+        return new JAXBElement<Division>(_Division_QNAME, Division.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link DivisionResponse }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://service.SOAP/", name = "divisionResponse")
+    public JAXBElement<DivisionResponse> createDivisionResponse(DivisionResponse value) {
+        return new JAXBElement<DivisionResponse>(_DivisionResponse_QNAME, DivisionResponse.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link AdditionResponse }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://service.SOAP/", name = "additionResponse")
+    public JAXBElement<AdditionResponse> createAdditionResponse(AdditionResponse value) {
+        return new JAXBElement<AdditionResponse>(_AdditionResponse_QNAME, AdditionResponse.class, null, value);
+    }
+
+}
